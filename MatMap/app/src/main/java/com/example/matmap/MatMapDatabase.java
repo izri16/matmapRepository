f32d6a20a542febcf440897124d98a2075cb137e
rizip1 <richardizip@gmail.com>
2015-03-25 18:54:58 +0100

    update: Added new projects for MatMap
    * Old projects should be in folder 'old' for now and only project will be MatMap

---
+++ MatMap/app/src/main/java/com/example/matmap/MatMapDatabase.java
@@ -0,0 +1,151 @@
+package com.example.matmap;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by richard on 5.2.2015.
+ */
+public class MatMapDatabase extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MatMapDatabase";
+
+    /*
+    V buducnosti sa bude vyuzivat pri prehladavani
+     */
+    private static final String ROOM_NAME = "room_name";
+    private static final String TOP_NEIGHBOR = "top_neighbor";
+    private static final String BOTTOM_NEIGHBOR = "bottom_neighbor";
+    private static final String LEFT_NEIGHBOR = "left_neighbor";
+    private static final String RIGHT_NEIGHBOR = "right_neighbor";
+
+
+    public MatMapDatabase(Context context) {
+        super(context, DATABASE_NAME, null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //db.execSQL("DROP TABLE IF EXISTS rooms");
+        db.execSQL("CREATE TABLE rooms (_id INTEGER PRIMARY KEY AUTOINCREMENT, room_name TEXT, top_neighbor TEXT, bottom_neighbor TEXT, left_neighbor TEXT, right_neighbor TEXT);");
+        //db.execSQL("DROP TABLE IF EXISTS history");
+        db.execSQL("CREATE TABLE history (_id INTEGER PRIMARY KEY AUTOINCREMENT, room_name TEXT, timestamp REAL);");
+        //db.execSQL("DROP TABLE IF EXISTS search_data");
+        db.execSQL("CREATE TABLE search_data (_id INTEGER PRIMARY KEY AUTOINCREMENT, room_name TEXT, " +
+                    "BSSID TEXT, strength REAL, device TEXT, SSID TEXT);");
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(ROOM_NAME, "M I");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M II");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M III");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M IV");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M V");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M VI");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M VII");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "A");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "B");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "C");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "F1");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "F2");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M 217");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "M 218");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+        cv.put(ROOM_NAME, "F1 109");
+        cv.put(TOP_NEIGHBOR, "nothing_for_now");
+        cv.put(BOTTOM_NEIGHBOR, "nothing_for_now");
+        cv.put(RIGHT_NEIGHBOR, "nothing_for_now");
+        cv.put(LEFT_NEIGHBOR, "nothing_for_now");
+        db.insert("rooms", ROOM_NAME, cv);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ MatMap/app/src/main/java/com/example/matmap/NewLocation.java
@@ -0,0 +1,144 @@
+package com.example.matmap;
+
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class NewLocation extends ActionBarActivity implements AdapterView.OnItemClickListener {
+    private AutoCompleteTextView inputField;
+    private SQLiteDatabase matMapDatabase = null;
+    private Cursor constantsCursor = null;
+    private List<String> items;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        //kvoli buggu triedy autocomplete lebo inak nevidno text
+        setTheme(android.R.style.Theme);
+        setContentView(R.layout.activity_new_location);
+
+        items = new ArrayList<>();
+        inputField = (AutoCompleteTextView)findViewById(R.id.newLocation);
+        inputField.setOnItemClickListener(this);
+
+        matMapDatabase = (new MatMapDatabase(this)).getWritableDatabase();
+        constantsCursor = matMapDatabase.rawQuery("SELECT room_name FROM rooms ORDER BY room_name", null);
+
+        constantsCursor.moveToFirst();
+        while(!constantsCursor.isAfterLast()) {
+            items.add(constantsCursor.getString(0));
+            constantsCursor.moveToNext();
+        }
+
+        inputField.setAdapter(new ArrayAdapter<String>(this, R.layout.auto_complete_text_view_item, R.id.autoCompleteItem,
+                items));
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        constantsCursor.close();
+        matMapDatabase.close();
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.new_location, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		return super.onOptionsItemSelected(item);
+	}
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String chosenRoom = (String) parent.getAdapter().getItem(position);
+        String title = "Added into history";
+        String message = "Will find shortest path in the future";
+
+        if (!addRoomIntoHistory(chosenRoom)) {
+            title = "Moved to first place in history";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .show();
+    }
+
+    public void openHelp(View view) {
+        Intent intent = new Intent(this, Help.class);
+        startActivity(intent);
+    }
+
+    private boolean addRoomIntoHistory(String room) {
+        ContentValues values = new ContentValues();
+        boolean answer = false;
+
+        if (!alreadyExists(room)) {
+            values.put("room_name", room);
+            values.put("timestamp", new Date().getTime());
+            matMapDatabase.insert("history", "room_name", values);
+            answer = true;
+        }
+        else {
+            String[] args = new String[]{room};
+            values.put("timestamp", new Date().getTime());
+            matMapDatabase.update("history", values, "room_name=?", args);
+        }
+
+        return answer;
+    }
+
+    private boolean alreadyExists(String room) {
+        boolean answer = false;
+        constantsCursor = matMapDatabase.rawQuery("SELECT room_name FROM history WHERE room_name = '" + room + "'", null);
+
+        constantsCursor.moveToFirst();
+
+        while(!constantsCursor.isAfterLast()) {
+            answer = true;
+            break;
+        }
+
+        return answer;
+    }
+}
